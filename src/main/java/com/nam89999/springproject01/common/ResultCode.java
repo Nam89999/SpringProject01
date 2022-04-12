0f99f7ee6bc61fed20cfa15d8b3507ba0fd629cd
이근주 <geunju.lee@navercorp.com>
2022-04-13 00:26:57 +0900

    git init

---
+++ src/main/java/com/nam89999/springproject01/common/ResultCode.java
@@ -0,0 +1,38 @@
+package com.nam89999.springproject01.common;
+
+public enum ResultCode {
+
+    Success(0, "성공"),
+
+
+    DBError(9998, "DB 오류입니다."),
+    ETCError(9999, "기타 오류입니다.");
+
+    ResultCode(int rtnCd, String rtnMsg) {
+        this.rtnCd = rtnCd;
+        this.rtnMsg = rtnMsg;
+    }
+
+    private int rtnCd;
+    private String rtnMsg;
+
+    public int getRtnCd() {
+        return rtnCd;
+    }
+
+    public String getRtnMsg() {
+        return rtnMsg;
+    }
+
+    public <T> Result<T> result() {
+        return new Result<>(null, this);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "rtnCd=" + rtnCd +
+                ", rtnMsg='" + rtnMsg + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/nam89999/springproject01/SpringProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nam89999.springproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
